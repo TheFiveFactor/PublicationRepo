c3cce2845f5901f7223bfbce2ccf40d51fbc834b
Guhan M <mguhan439@gmail.com>
2022-05-11 22:09:53 +0530

    added browse page

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 from repository import app, db
-from repository.models import User, Role, Institution, Department, DepartmentAreas, PaperType, PublishPaper
+from repository.models import User, Role, Institution, Department, DepartmentAreas, PaperType, PublishPaper, author_publish_paper
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Role': Role, 'Institution': Institution,
-        'Department': Department, 'DepartmentAreas': DepartmentAreas, 'PaperType': PaperType, 'PublishPaper': PublishPaper}
+        'Department': Department, 'DepartmentAreas': DepartmentAreas, 'PaperType': PaperType, 'PublishPaper': PublishPaper,
+            'author_publish_paper': author_publish_paper}
 
 if __name__ == '__main__':
     # app.run()
